56c1ae3ff3cb213f04867bddf2df5e556a72b2cd
LadyT-2005 <titilopemioyinlola@gmail.com>
2022-06-11 07:53:38 -0700

    commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * Description: compute absolute value of integer
+ * Return: 0
+ * @a: checker to be checked.
+ */
+int _abs(int a)
+{
+	if (_abs(a < 0))
+	{
+		_putchar(a);
+	}
+	else if (_abs(a > 0))
+	{
+		_putchar(a);
+	}
+	else
+	{
+		_putchar('0');
+	}
+return (0);
+}
