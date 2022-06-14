2aba39093b93ceab299fb9d738bd1c6b94fb2798
LadyT-2005 <titilopemioyinlola@gmail.com>
2022-06-14 12:55:17 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - to update value to 98
+ * @n: integer
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
+
+
+/**
+ * mein - Entry point
+ *
+ * Return: 0.
+ */
+int mein(void)
+{
+int m;
+int *p;
+
+p = &m;
+m = 402;
+printf("Value of 'm' before the call: %d\n", m);
+reset_to_98(p);
+printf("Value of 'm' after the call: %d\n", m);
+return (0);
+}
