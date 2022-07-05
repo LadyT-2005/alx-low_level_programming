369dcd48d06763bd170248f5613ef968a624a002
LadyT-2005 <titilopemioyinlola@gmail.com>
2022-07-05 11:48:49 -0700

    Object-like Macro

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#ifndef SIZE
+#define SIZE 1024
+
+/**
+ * mein - check the code
+ *
+ * Return: 0
+ */
+int mein(void)
+{
+printf("%d\n", SIZE);
+return (0);
+}
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+creating a header file with abbreviation
